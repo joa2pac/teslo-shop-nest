52e6697ee22dc550a6ab954bae0e3471de47d8ea
joaquin <joaquinahuel93@gmail.com>
2024-06-13 08:51:54 -0300

    add atributes in product entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -11,4 +11,34 @@ id: string;
 })
 title: string;
 
+@Column('numeric', {
+    default: 0
+})
+price: number;
+
+@Column({
+    type: 'text',
+    nullable: true,
+})
+description: string
+
+@Column('text', {
+    unique: true,
+})
+slug: string
+
+@Column('int', {
+    default: 0,
+})
+stock: number;
+
+@Column('text',{
+    array: true,
+})
+sizes: string[]
+
+
+@Column('text')
+gender: string;
+
 }
