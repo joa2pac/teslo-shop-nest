8f3229089d73fd22508306126974aeebbcef60f2
joaquin <joaquinahuel93@gmail.com>
2024-06-20 14:24:49 -0300

    add login user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
@@ -6,6 +6,7 @@ import * as bcrypt from 'bcrypt'
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import { LoginUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -37,6 +38,28 @@ constructor(
     }
     
   }
+
+
+async login(loginUserDto: LoginUserDto) {
+
+
+  const {email, password} = loginUserDto;
+
+  const user = await this.userRepository.findOne({
+    where: { email },
+    select: { email: true, password: true }
+  });
+
+  if( !user ) 
+    throw new UnauthorizedException('Not valid credentials(email)');
+  
+
+  if ( !bcrypt.compareSync( password, user.password ) )
+    throw new UnauthorizedException('Not valid credentials(password)');
+
+  return user;
+
+}
 
   private handleDBerrors(error: any): never {
 
