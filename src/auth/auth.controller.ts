862c4acc596b5eff607044fb67825b3afde58ca9
joaquin <joaquinahuel93@gmail.com>
2024-06-21 16:16:37 -0300

    add custom guard 'user-role'

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto'
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser, RawHeaders } from './decorators'
 import { User } from './entities/user.entity';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
 
 
 
@@ -33,7 +34,7 @@ export class AuthController {
     @RawHeaders() rawHeaders: string[]
   ) {
 
-    // console.log({request})
+    
 
     return {
       ok: true,
@@ -43,4 +44,20 @@ export class AuthController {
      rawHeaders
     }
   }
+
+
+  @Get('private2')
+  @SetMetadata('roles', ['admin', 'super-user'])
+  @UseGuards( AuthGuard(), UserRoleGuard )
+  privateRoute2(
+    @GetUser() user: User,
+  ) {
+
+      return {
+        ok: true,
+        user
+      }
+
+  }
+
 }
