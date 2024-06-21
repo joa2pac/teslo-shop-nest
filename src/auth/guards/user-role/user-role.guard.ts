3f963f87b12771b3c56c977a16dca4303095e99d
joaquin <joaquinahuel93@gmail.com>
2024-06-21 16:34:38 -0300

    check user role

--- src/auth/guards/user-role/user-role.guard.ts
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -1,6 +1,7 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { User } from 'src/auth/entities/user.entity';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -15,8 +16,23 @@ export class UserRoleGuard implements CanActivate {
 
    const validRoles: string[] = this.reflector.get( 'roles', context.getHandler() );
 
-   console.log({ validRoles });
+   if (!validRoles) return true;
+   if (validRoles.length === 0) return true;
 
-    return true;
+   const req = context.switchToHttp().getRequest();
+   const user = req.user as User;
+
+   if ( !user ) 
+    throw new InternalServerErrorException('User not found');
+
+   for ( const role of user.roles ) {
+    if (validRoles.includes( role ) ) {
+      return true;
+    }
+   }
+
+    throw new ForbiddenException(
+      `User ${ user.fullName } need a valid role: [${ validRoles }]`
+    );
   }
 }
